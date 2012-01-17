bedd3e821b09844e4ba17160817710f05833e5cf
Eric Stokes <fernferret@gmail.com>
2012-01-16 18:29:48 -0700

    Add EconXP Bank

---
+++ src/main/java/com/fernferret/allpay/EconXPBank.java
@@ -0,0 +1,49 @@
+package com.fernferret.allpay;
+
+import ca.agnate.EconXP.EconXP;
+import org.bukkit.entity.Player;
+
+public class EconXPBank extends GenericBank {
+	private EconXP plugin;
+
+	public EconXPBank(EconXP plugin) {
+		this.plugin = plugin;
+	}
+
+	@Override
+	public String getFormattedMoneyAmount(Player player, double amount) {
+		return this.formatCurrency(amount, "XP", "XP");
+	}
+
+	@Override
+	public boolean hasMoney(Player player, double money, String message) {
+		boolean result = this.plugin.getExp(player) >= money;
+		if (!result) {
+			userIsTooPoor(player, -1, message);
+		}
+		return result;
+	}
+
+	@Override
+	public void payMoney(Player player, double amount) {
+		this.plugin.removeExp(player, (int) amount);
+		showReceipt(player, amount, -1);
+	}
+
+	@Override
+	public String getEconUsed() {
+		return "EconXP";
+	}
+
+    @Override
+    protected double getMoneyBalance(Player p) {
+        return this.plugin.getExp(p);
+    }
+
+    @Override
+    protected void giveMoney(Player player, double amount) {
+        this.plugin.addExp(player, (int)amount);
+        showReceipt(player, (amount * -1), -1);
+    }
+
+}
