7b04fd6f03deb33a0f9dfbdd746ba58b08edbf26
Jeffrey Wardian <krinsdeath@hotmail.com>
2013-03-09 10:06:19 -0800

    Refactored MetroPlugin loader for possible initialization order errors.

--- modules/fe-0.7/src/main/java/com/fernferret/allpay/economy/FeconomyBank7.java
+++ modules/fe-0.7/src/main/java/com/fernferret/allpay/economy/FeconomyBank7.java
@@ -16,18 +16,11 @@ import java.util.logging.Level;
  */
 public class FeconomyBank7 extends GenericBank {
     private final FeBukkitLoader plugin;
-    private final Fe service;
+    private Fe service;
 
     public FeconomyBank7(Plugin plugin) {
         this.plugin = (FeBukkitLoader) plugin;
-        try {
-            Field field = FeBukkitLoader.class.getDeclaredField("fe");
-            field.setAccessible(true);
-            this.service = (Fe) field.get(this.plugin);
-        } catch (Exception e) {
-            this.plugin.getLogger().log(Level.SEVERE, e.getLocalizedMessage(), e);
-            throw new IncompleteBankException("Couldn't get API instance for Fe-economy");
-        }
+        getFe();
     }
 
     @Override
@@ -35,14 +28,29 @@ public class FeconomyBank7 extends GenericBank {
         return "Fe";
     }
 
+    private void getFe() {
+        if (this.service == null) {
+            try {
+                Field field = FeBukkitLoader.class.getDeclaredField("fe");
+                field.setAccessible(true);
+                this.service = (Fe) field.get(this.plugin);
+            } catch (Exception e) {
+                this.plugin.getLogger().log(Level.SEVERE, e.getLocalizedMessage(), e);
+                throw new IncompleteBankException("Couldn't get API instance for Fe-economy");
+            }
+        }
+    }
+
     @Override
     protected boolean hasMoney(Player player, double money, String message) {
+        getFe();
         Account acc = this.service.getAPI().getAccount(player.getName());
         return acc != null && acc.has(money);
     }
 
     @Override
     protected void takeMoney(Player player, double amount) {
+        getFe();
         Account acc = this.service.getAPI().getAccount(player.getName());
         if (acc != null) {
             acc.withdraw(amount);
@@ -51,6 +59,7 @@ public class FeconomyBank7 extends GenericBank {
 
     @Override
     protected void giveMoney(Player player, double amount) {
+        getFe();
         Account acc = this.service.getAPI().getAccount(player.getName());
         if (acc != null) {
             acc.deposit(amount);
@@ -59,11 +68,13 @@ public class FeconomyBank7 extends GenericBank {
 
         @Override
     protected String getFormattedMoneyAmount(Player player, double amount) {
+        getFe();
         return this.service.getAPI().formatNoColor(amount);
     }
 
     @Override
     protected boolean setMoneyBalance(Player player, double amount) {
+        getFe();
         try {
             Account acc = this.service.getAPI().getAccount(player.getName());
             acc.setMoney(amount);
@@ -76,6 +87,7 @@ public class FeconomyBank7 extends GenericBank {
 
     @Override
     protected double getMoneyBalance(Player player) {
+        getFe();
         Account acc = this.service.getAPI().getAccount(player.getName());
         if (acc != null) {
             return acc.getMoney();
